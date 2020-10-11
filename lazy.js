aceddd708658c3454c27b1cda21ecf50212ab1e6
SlamDunk <stepanov.arkadyushka@inbox.ru>
2020-10-11 12:54:25 +0400

    Исправление и измение LazyLoading

--- lazy.js
+++ lazy.js
@@ -1,28 +1,24 @@
 window.addEventListener('load', () => { //Ну да, если навесить событие на document, то ничего не случиться)
 
    let mainChildrens = document.querySelectorAll('.main-lazy__childrens');
-
-   let loadingImg = "img/loading_screen.jpg";
    
    let options = {
    	 root: null,
    	 rootMargin: "0px",
-     thershold: 0.6
+     threshold: 0.5
    }; //Если устраивают root и rootMargin, то можно их и не писать
 
    let callback = (entries, observer) => {
       entries.forEach(entry => {
-        let img = entry.target.children[0];  
+        let img = entry.target.children[0];
 
-      	if(entry.isIntersecting && img.classList.contains('loading')) { 
-           img.src = img.dataset.src;
+      	if(entry.isIntersecting && !entry.target.classList.contains('loading')) {
+           entry.target.classList.add('loading');
 
-           img.classList.remove('loading');
-      	} else {
-      	   img.src = loadingImg;
-
-      	   img.classList.add('loading');
-      	}
+           document.title = img.getAttribute("alt"); //Смена title у всей веб-страницы
+        } else {
+           entry.target.classList.remove('loading');
+        }
       });
    }
 
